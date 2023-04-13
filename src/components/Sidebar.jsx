0d0473fe4f4b0f4ab3ba27d4272e3f0e8449181e
MarcGriese <marcgriese76@gmail.com>
2023-04-13 11:24:52 +0200

    changes to sidebar and imports

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../ressource/assets/sidebar.css'
+import Logo from '../ressource/icons/logo.png'
+import HomeIcon from '../ressource/icons/Home.png'
 //1200x675
 //width 1/5
 //height: 61/675
@@ -12,20 +14,64 @@ export default function Sidebar() {
     return (
         <div className="sidebar-background">
             <div className="sidebar-mainicon__container">
-                <img src="" alt="" className="sidebar-mainicon" />
+                <img src={Logo} alt="dhbworkout logo" className="sidebar-mainicon" />
             </div>
 
             <div className="sidebar-menu">
 
                 <div className="sidebar-button">
-                    <div className="sidebar-icon-container">
-                        <img src="" alt="" className="sidebar-icon" />
+                    <div className="sidebar-icon__container">
+                        <img src={HomeIcon} alt="home icon" className="sidebar-icon" />
                     </div>
                     <div className="sidebar-string">
-                        <span></span>
+                        <span className='aileron-bold-white-18px'>Home</span>
                     </div>
                 </div>
 
+                <div className="sidebar-button">
+                    <div className="sidebar-icon__container">
+                        <img src={HomeIcon} alt="home icon" className="sidebar-icon" />
+                    </div>
+                    <div className="sidebar-string">
+                        <span className='aileron-bold-white-18px'>Profile</span>
+                    </div>
+                </div>
+
+                <div className="sidebar-button">
+                    <div className="sidebar-icon__container">
+                        <img src={HomeIcon} alt="home icon" className="sidebar-icon" />
+                    </div>
+                    <div className="sidebar-string">
+                        <span className='aileron-bold-white-18px'>Plan</span>
+                    </div>
+                </div>
+
+                <div className="sidebar-button">
+                    <div className="sidebar-icon__container">
+                        <img src={HomeIcon} alt="home icon" className="sidebar-icon" />
+                    </div>
+                    <div className="sidebar-string">
+                        <span className='aileron-bold-white-18px'>Plan</span>
+                    </div>
+                </div>
+
+                <div className="sidebar-button">
+                    <div className="sidebar-icon__container">
+                        <img src={HomeIcon} alt="home icon" className="sidebar-icon" />
+                    </div>
+                    <div className="sidebar-string">
+                        <span className='aileron-bold-white-18px'>Ernährung</span>
+                    </div>
+                </div>
+
+                <div className="sidebar-button">
+                    <div className="sidebar-icon__container">
+                        <img src={HomeIcon} alt="home icon" className="sidebar-icon" />
+                    </div>
+                    <div className="sidebar-string">
+                        <span className='aileron-bold-white-18px'>Settings</span>
+                    </div>
+                </div>
 
             </div>
         </div>
