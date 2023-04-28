b6a54cf5657d30333e6e709cedb753c9f81c80a9
MarcGriese <marcgriese76@gmail.com>
2023-04-28 13:30:25 +0200

    example for sidebar for login and registration pdf

--- src/pages/PDFsLogin/KontaktLogin.jsx
+++ src/pages/PDFsLogin/KontaktLogin.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import FooterLong from '../../components/l_Footer_long'
 import PDFViewer from '../../components/PDFViewerKontakt'
 import '../../ressource/assets/loginview.css'
+import Mainicon from '../../ressource/icons/logo.png'
 
 import Arrow from '../../ressource/icons/arrow-right-line.png'
 
@@ -10,9 +11,13 @@ export default function KontaktLogin() {
     return (
         <div className='fullview'>
             <Link to='/login' className='login-sidebar'>
+                <div className="l-mainicon_container">
+                    <img src={Mainicon} alt="dhbworkout logo" className='l-mainicon'/>
+                </div>
                 <div className="icon_container">
                     <img src={Arrow} alt="back arrow" className='arrow' />
                 </div>
+                <p className='aileron-bold-white-24px'>Zurück</p>
             </Link>
             <PDFViewer />
             <FooterLong />
