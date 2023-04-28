29e979124478e58c178ff6eb939207266c637c87
Schmidt, Alex <alex.schmidt@berthold.com>
2023-04-28 13:26:59 +0200

    Added new Page for the bmi-calculator

---
+++ src/pages/Calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+    return (
+        <div>
+            <Sidebar />
+            <h1>BMI-Calculator</h1>
+            <Footer />
+        </div>
+    )
+}
