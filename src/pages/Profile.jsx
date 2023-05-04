99bf2a5fb5ddb1ab6263e1bc673e0a9f0a6edbaa
Richone01 <leonrichter01@gmail.com>
2023-05-04 13:07:03 +0200

    Implemented js function to read cookies

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Footer from '../components/Footer'
-//import '../ressource/assets/calculater.css'
+import getCookie from "../Cookies";
 
 export default function Profile() {
 
@@ -28,21 +28,6 @@ export default function Profile() {
         }
     };
 
-    function getCookie(cname) {
-        let name = cname + "=";
-        let decodedCookie = decodeURIComponent(document.cookie);
-        let ca = decodedCookie.split(';');
-        for(let i = 0; i <ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) === ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) === 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
     getData();
     return (
         
