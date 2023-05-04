98e1dac4d2ec78b3043d77df37b8e971241157a9
Richone01 <leonrichter01@gmail.com>
2023-05-04 15:44:06 +0200

    Changed permissions for test-result directory

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ npm start &
 npx playwright install
 npx playwright install-deps
 npx playwright test -c ./playwright.config.ts
-chmod -R 777 /dhbworkout-webapp-test-results
+chmod -R 777 /app/test-results
